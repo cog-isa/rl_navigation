c0719c3a5cfc80ff65d7549848f7f0b2d5852dfb
Stepan-Makarenko <makarenko.sd@phystech.edu>
2020-01-27 14:03:04 +0300

    resnet18 embeding

--- makarenko/Model.py
+++ makarenko/Model.py
@@ -16,17 +16,6 @@ class Encoder(nn.Module):
             features.append(layer(features[-1]))
         return features
 
-#     def __init__(self):
-#         super(Encoder, self).__init__()       
-#         import torchvision.models as models
-#         self.original_model = models.densenet161( pretrained=True )
-        
-#     def forward(self, x):
-#         features = [x]
-#         for k, v in self.original_model.features._modules.items(): features.append( v(features[-1]) )
-#         return features
-
-
 
 class UpSample(nn.Module):
     def __init__(self, in_size, out_size):
@@ -73,29 +62,6 @@ class Decoder(nn.Module):
         out = self.conv2(out)
 
         return out
-
-# class Decoder(nn.Module):
-#     def __init__(self, num_features=2208, decoder_width = 0.5):
-#         super(Decoder, self).__init__()
-#         features = int(num_features * decoder_width)
-
-#         self.conv2 = nn.Conv2d(num_features, features, kernel_size=1, stride=1, padding=1)
-
-#         self.up1 = UpSample(features//1 + 384, features//2)
-#         self.up2 = UpSample(features//2 + 192, features//4)
-#         self.up3 = UpSample(features//4 +  96, features//8)
-#         self.up4 = UpSample(features//8 +  96, features//16)
-
-#         self.conv3 = nn.Conv2d(features//16, 1, kernel_size=3, stride=1, padding=1)
-
-#     def forward(self, features):
-#         x_block0, x_block1, x_block2, x_block3, x_block4 = features[3], features[4], features[6], features[8], features[11]
-#         x_d0 = self.conv2(x_block4)
-#         x_d1 = self.up1(x_d0, x_block3)
-#         x_d2 = self.up2(x_d1, x_block2)
-#         x_d3 = self.up3(x_d2, x_block1)
-#         x_d4 = self.up4(x_d3, x_block0)
-#         return self.conv3(x_d4)
 
 
 class Model(nn.Module):
